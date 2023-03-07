4bff163fb5c3c6ff18d84749be7699041551c38b
Maggy <brend.protasio@edu.cobianchi.it>
2023-03-07 12:16:27 +0100

    : ADD partita class

---
+++ src/Partita.java
@@ -0,0 +1,50 @@
+public class Partita {
+    public int puntiTotali;
+    public Utente utente;
+    public String parolaMigliore;
+
+    //costruttore
+    public Partita(Utente u){
+        this.Utente = utente;
+        this.puntiTotali = 0;
+        this.parolaMigliore = null;
+    }
+
+    // getter e setter
+    public int getPuntiTotali() {
+        return puntiTotali;
+    }
+    public void setPuntiTotali(int puntiTotali) {
+        this.puntiTotali = puntiTotali;
+    }
+    public Utente getUtente() {
+        return utente;
+    }
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+    public String getParolaMigliore() {
+        return parolaMigliore;
+    }
+    public void setParolaMigliore(String parolaMigliore) {
+        this.parolaMigliore = parolaMigliore;
+    }
+
+    //metodi
+    public void aggiungiPunti(int lunghezzaParola){
+        int k = 100;            //costante
+        int punti = lunghezzaParola * k;
+        puntiTotali = puntiTotali + punti;
+    }
+    public boolean controllaParolaMigliore(String parola){
+        if(parola.length() > parolaMigliore.length()){
+            parolaMigliore = parola;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+}
