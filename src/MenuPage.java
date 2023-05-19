9b488f1212bd002c3c249fe10e9c2e8d0a14203d
Maggy <brend.protasio@edu.cobianchi.it>
2023-05-19 14:35:12 +0200

    ADD: Design for start, menu and easy mode page

--- src/MenuPage.java
+++ src/MenuPage.java
@@ -6,21 +6,34 @@ public class MenuPage extends JFrame {
     private JPanel panelMain;
     private JLabel image;
     private JPanel panelOut;
-    private JButton classicoBtn;
-    private JButton timerBtn;
+    private JButton easyModeBtn;
+    private JButton specialBtn;
+    private JButton mediumModeBtn;
+    private JButton hardModeBtn;
+    private JButton backBtn;
 
     public MenuPage(Utente giocatore){
         // Set the properties of the frame
         setContentPane(panelMain);
         setTitle("Menu Page");
-        setSize(760,560);
+        setSize(1000,700);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        classicoBtn.addActionListener(new ActionListener() {
+        easyModeBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                ClassicoGame g = new ClassicoGame(giocatore);
+                //crea partita
+                Partita partita = new Partita(giocatore);
+                EasyMode g = new  EasyMode(giocatore, partita);
                 g.setVisible(true);
+                dispose();
+            }
+        });
+        backBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StartPage start = new StartPage();
+                start.setVisible(true);
                 dispose();
             }
         });
