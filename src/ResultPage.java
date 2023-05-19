dd5804d36c5b8bd9fed899f4b4e0441f6ac4d7fe
alice <alice.minuzzo@edu.cobianchi.it>
2023-05-19 17:42:08 +0200

    FIX:implemento ResultPage

--- src/ResultPage.java
+++ src/ResultPage.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class ResultPage {
+    private JButton retryButton; // manda alla pagina di menu
+    private JButton exitButton; //manda alla home
+    private JPanel labelPunti;
+
+    private void createUIComponents() {
+
+    }
 }
