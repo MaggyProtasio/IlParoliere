86a350991f948594714be2f666d04a69958233ec
alice <alice.minuzzo@edu.cobianchi.it>
2023-05-21 18:04:16 +0200

    FIX: grafica ResultPage

--- src/ResultPage.java
+++ src/ResultPage.java
@@ -15,7 +15,7 @@ public class ResultPage extends JFrame {
     public ResultPage(Partita p, Utente g){
         setContentPane(panelMain);
         setTitle("WordCraft - Results");
-        setSize(900,600);
+        setSize(800,600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //modifica label punteggio
